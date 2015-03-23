9d84d32d576c977520197a6cf510c49bc19c2e16
dtran <dzung.d.tran@intel.com>
2015-03-23 16:14:32 -0700

    Added api documentation for account management module.

--- IoTKitLib/IoTKitLib/AccountManagement.h
+++ IoTKitLib/IoTKitLib/AccountManagement.h
@@ -27,22 +27,82 @@
  */
 @interface AccountManagement : DefaultConfiguration
 
+/*!
+ * Create an account with a name.
+ *
+ * @param accountName name of the account to be created.
+ * @return For async model, return CloudResponse which wraps true if the request of REST
+ * call is valid; otherwise false. The actual result from
+ * the REST call is return asynchronously as part HttpResponseDelegatee of DefaultConfiguration.
+ * For synch model, return CloudResponse which wraps HTTP return code and response.
+ */
 -(CloudResponse *) createAnAccount:(NSString*)accountName;
+
+/*!
+ * Get the information about an account.
+ *
+ * @return For async model, return CloudResponse which wraps true if the request of REST
+ * call is valid; otherwise false. The actual result from
+ * the REST call is return asynchronously as part HttpResponseDelegatee of DefaultConfiguration.
+ * For synch model, return CloudResponse which wraps HTTP return code and response.
+ */
 -(CloudResponse *) getAccountInformation;
+
 /*!
  * Get the account activation code which is the transient code that can be used to activate
  * devices for the account. It expires after one hour.
  *
  * @return For async model, return CloudResponse which wraps true if the request of REST
  * call is valid; otherwise false. The actual result from
- * the REST call is return asynchronously as part {@link RequestStatusHandler#readResponse}.
+ * the REST call is return asynchronously as part HttpResponseDelegatee of DefaultConfiguration.
  * For synch model, return CloudResponse which wraps HTTP return code and response.
  */
 -(CloudResponse *) getAccountActivationCode;
+
+/*!
+ * Force a renewal of the account activation code.
+ *
+ * @return For async model, return CloudResponse which wraps true if the request of REST
+ * call is valid; otherwise false. The actual result from
+ * the REST call is return asynchronously as part HttpResponseDelegatee of DefaultConfiguration.
+ * For synch model, return CloudResponse which wraps HTTP return code and response.
+ */
 -(CloudResponse *) renewAccountActivationCode;
+
+/*!
+ * Update the account with a list of attributes
+ *
+ * @param accountNameToUpdate the name that identifies the account to be updated
+ * @param atttributes the optional list of name value pair of attributes to be updated
+ * @return For async model, return CloudResponse which wraps true if the request of REST
+ * call is valid; otherwise false. The actual result from
+ * the REST call is return asynchronously as part HttpResponseDelegatee of DefaultConfiguration.
+ * For synch model, return CloudResponse which wraps HTTP return code and response.
+ */
 -(CloudResponse *) updateAnAccount:(NSString*)accountNameToUpdate andOptionalAttributesWithSimpleKeyValues:(NSDictionary*)attributes;
+
+/*!
+ * Add another user to your account.
+ *
+ * @param accountId     The account id of the other user.
+ * @param inviteeUserId The user id of the other user.
+ * @param isAdmin       The role for this user in the current account.
+ * @return For async model, return CloudResponse which wraps true if the request of REST
+ * call is valid; otherwise false. The actual result from
+ * the REST call is return asynchronously as part HttpResponseDelegatee of DefaultConfiguration.
+ * For synch model, return CloudResponse which wraps HTTP return code and response.
+ * @throws JSONException
+ */
 -(CloudResponse *) addAnotherUserToAccount:(NSString*)accountId UserGettingInvited:(NSString*)inviteeUserId
                            Admin:(BOOL)isAdmin;
+/*!
+ * Delete the current account.
+ *
+ * @return For async model, return CloudResponse which wraps true if the request of REST
+ * call is valid; otherwise false. The actual result from
+ * the REST call is return asynchronously as part HttpResponseDelegatee of DefaultConfiguration.
+ * For synch model, return CloudResponse which wraps HTTP return code and response.
+ */
 -(CloudResponse *) deleteAnAccount;
 
 @end
