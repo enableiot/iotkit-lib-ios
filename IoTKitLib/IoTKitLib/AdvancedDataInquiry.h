af7c0913b791e908545b00fa259692218938aa31
dtran <dzung.d.tran@intel.com>
2015-03-24 14:55:52 -0700

    Added api documentation for Advanced Data Inquiry module.

--- IoTKitLib/IoTKitLib/AdvancedDataInquiry.h
+++ IoTKitLib/IoTKitLib/AdvancedDataInquiry.h
@@ -24,23 +24,108 @@
 #import "DefaultConfiguration.h"
 #import "AttributeFilter.h"
 
+/*!
+ * Advanced Data Inquiry allows querying measurement data (values, location and attributes) for
+ * a single account using advanced filtering and sorting.
+ */
 @interface AdvancedDataInquiry : DefaultConfiguration
 
 -(id) init __attribute__((unavailable("Must create object using \"initAdvancedDataInquiryWithDefaults\" method")));
+
+/*!
+ * Creates custom instance of the class AdvancedDataEnquiry
+ *
+ * @return an instance of the class AdvancedDataEnquiry
+ */
 -(id)initAdvancedDataInquiryWithDefaults;
+
+/*!
+ * Append gatewayId to list
+ * @param gatewayId The gateway identifier
+ */
 -(void)addGatewayId:(NSString*)gatewayId;
+
+/*!
+ * Append deviceId method to list
+ * @param deviceId The device identifier
+ */
 -(void)addDeviceId:(NSString*)deviceId;
+
+/*!
+ * Append componentId method to list
+ * @param componentId The component identifier
+ */
 -(void)addComponentId:(NSString*)componentId;
--(void)setStartTimestamp:(long)startTimestamp;
--(void)setEndTimestamp:(long)endTimestamp;
+
+/*!
+ * Set start time stamp
+ * @param startTimestamp The starting time
+ */
+-(void)setStartTimestamp:(long)timestamp;
+
+/*!
+ * Set end time stamp
+ * @param endTimestamp The ending time
+ */
+-(void)setEndTimestamp:(long)timestamp;
+
+/*!
+ * Append measure attribute to list
+ * @param attribute The measured attribute
+ */
 -(void)addReturnedMeasureAttributes:(NSString*)attribute;
+
+/*!
+ * Set whether to measure location on data inquiry
+ * @param measureLocation True if measured location to be returned; otherwise false
+ */
 -(void)setShowMeasureLocation:(BOOL)measureLocation;
+
+/*!
+ * Append device comp attribute filter to list. Filters by device and/or component attributes.
+ * @param attributeFilter The filter to filtered by
+ */
 -(void)addDevCompAttributeFilter:(AttributeFilter*)attributeFilter;
+
+/*!
+ * Append measurement attribute filter to list. Filters by measurement attributes.
+ * @param attributeFilter The filter to filtered by
+ */
 -(void)addMeasurementAttributeFilter:(AttributeFilter*)attributeFilter;
+
+/*!
+ * Append avalue filter to list. Filters by measurement values.
+ * @param attributeFilter The filter to filtered by
+ */
 -(void)addValueFilter:(AttributeFilter*)attributeFilter;
--(void)setComponentRowLimit:(NSInteger)componentRowLimit;
+
+/*!
+ * Set component row limit for return results
+ * @param rowLimit The number of row to return
+ */
+-(void)setComponentRowLimit:(NSInteger)rowLimit;
+
+/*!
+ * Set count true to get only count from server else false
+ * @param countOnly True if query contains count; otherwise false
+ */
 -(void)setCountOnly:(BOOL)countOnly;
+
+/*!
+ * Append sort key-value pair to list for query to be sorted by
+ * @param name The name of the key
+ * @param value The value of the key
+ */
 -(void)addSortInfo:(NSString*)name WithValue:(NSString*)value;
+
+/**
+ * Starts a request for the report.
+ *
+ * @return For async model, return CloudResponse which wraps true if the request of REST
+ * call is valid; otherwise false. The actual result from
+ * the REST call is return asynchronously as part {@link RequestStatusHandler#readResponse}.
+ * For synch model, return CloudResponse which wraps HTTP return code and response.
+ */
 -(CloudResponse *)request;
 
 
